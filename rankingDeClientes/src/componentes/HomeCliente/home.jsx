1671e050c28e3615da11821ad906e1e92b8e4398
Davi Assunção dos Santos <82240058+davi011205@users.noreply.github.com>
2024-10-31 17:57:54 -0300

    mobile first
    - histórico como menu lateral
    - tabela princiapl mais organizada
    - foco no mobile

--- rankingDeClientes/src/componentes/HomeCliente/home.jsx
+++ rankingDeClientes/src/componentes/HomeCliente/home.jsx
@@ -65,37 +65,45 @@ const HomeCliente = () => {
   return (
     <div className="homeContainer">
       <header>
-          <button onClick={() => setShowHistorico(!showHistorico)}>Ver histórico de pontos</button>
-          <button>Sair</button>
+        <div className='historico' onClick={() => setShowHistorico(!showHistorico)}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
+          {showHistorico && (
+          <div className="displayHistorico">
+            <div className="historicoPontos">
+              <div className="tituloHistorico">
+                <h3>Histórico de Pontos</h3>  
+              </div>
+              <div className="tabelaHistorico">
+                {historicoPontos.length === 0 ? (
+                  <p>Não há histórico de pontos disponível.</p>
+                ) : (
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Data da Compra</th>
+                        <th>Pontos</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {historicoPontos.map((item, index) => (
+                        <tr key={index}>
+                          <td>{item.dataPonto}</td>
+                          <td>{item.quantidadePonto}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                )}
+              </div>
+            </div>
+          </div>
+          )}
+        </div>
           <h2>Promoção Top 10 de natal</h2>
+          <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z"/></svg></div>
       </header>
 
       <main>
-        {showHistorico && (
-        <div className="historicoPontos">
-          <h3>Histórico de Pontos</h3>
-          {historicoPontos.length === 0 ? (
-            <p>Não há histórico de pontos disponível.</p>
-          ) : (
-            <table>
-              <thead>
-                <tr>
-                  <th>Data da Compra</th>
-                  <th>Pontos</th>
-                </tr>
-              </thead>
-              <tbody>
-                {historicoPontos.map((item, index) => (
-                  <tr key={index}>
-                    <td>{item.dataPonto}</td>
-                    <td>{item.quantidadePonto}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
-        </div>
-        )}
         <div className="rankinkPodio"></div>
         <div className="ranking">
           <h3>Ranking de Clientes</h3>
